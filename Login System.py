d65f258e1de930462002f3d94bd40b3eaa73d799
jettilicious <tion.jetclev@gmail.com>
2026-05-15 22:04:35 +0800

    Added aditional registration checks

--- Login System.py
+++ Login System.py
@@ -1,27 +1,24 @@
 from tkinter import *
 from tkinter import messagebox
-
+import re
 
 def login():
     username = entry_login_user.get()
     password = entry_login_pass.get()
 
     if not username.strip() or not password.strip():
-        messagebox.showerror("Error", "Please enter both username and password.")
+        messagebox.showerror("Error", "Please enter both username and password!")
         return
 
     try:
         with open("Credentials.txt", "r") as f:
             accounts = f.readlines()
-
         for account in accounts:
             saved_user, saved_pass = account.strip().split(",")
-
             if username == saved_user and password == saved_pass:
                 messagebox.showinfo("Login Sucess", "Welcome")
                 return
-
-        messagebox.showerror("Login Failed", "Incorrect Username or Password.")
+        messagebox.showerror("Login Failed", "Incorrect Username or Password!")
 
     except FileNotFoundError:
         messagebox.showerror("Error", "No Accounts found.")
@@ -30,9 +27,21 @@ def login():
 def register():
     username = entry_login_user.get()
     password = entry_login_pass.get()
+    specialChar = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+
     if not username.strip() or not password.strip():
-        messagebox.showerror("Error", "Please enter both username and password.")
+        messagebox.showerror("Error", "Please enter both username and password!")
         return
+    elif len(username.strip()) < 6 or len(password.strip()) < 6:
+        messagebox.showerror("Error", "Username/Password must be at least 6 characters long!")
+        return
+    elif not specialChar.search(password):
+        messagebox.showerror("Error", "Password has no special character!")
+        return
+    elif not any(char.isdigit() for char in password):
+        messagebox.showerror("Error", "Password must contain at least 1 digit")
+        return
+    
     with open("Credentials.txt", "a") as f:
         f.write(f"{username},{password}\n")
     messagebox.showinfo("Success", "Account saved successfully!")
@@ -41,7 +50,7 @@ def register():
 
 
 root = Tk()
-root.title("=== Login System ===")
+root.title("Login System")
 root.geometry("500x300")
 
 label_login_user = Label(root, text="Username:")
